daff1c169b9dfb615ef8f3d04df20e2f6103b9fb
kakishkak <kolya.yokimavich@gmail.com>
2024-11-27 18:52:09 +0200

    Update constants24.cpp

--- constants24/constants24.cpp
+++ constants24/constants24.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class User {
+    string name;
+    string surname;
+    int age;
+public:
+    static int amountOfUsers;
+    User() {
+        IncrementAmountOfPoints(1);
+    }
+
+    static void IncrementAmountOfPoints(int x) {
+        amountOfUsers += x;
+    }
+};
+
+int User::amountOfUsers = 0;
+
+int main() {
+    User obj;
+    cout << User::amountOfUsers << endl;
+    User obj1;
+    cout << User::amountOfUsers << endl;
+    User obj2;
+    cout << User::amountOfUsers << endl;
 }
